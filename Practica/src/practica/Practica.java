618c58b629f2d4256b20fa3bb7038754317906a2
Traya <Traya@Traya>
2021-01-20 15:21:48 +0100

    Validacion del tamaño variable mediante una funcion

--- Practica/src/practica/Practica.java
+++ Practica/src/practica/Practica.java
@@ -19,17 +19,19 @@ public class Practica {
     public static void main(String[] args) {
         // TODO code application logic here
         int size;
+        boolean valsize;
         Scanner sc = new Scanner(System.in);
-        do{
-        System.out.println("Cuantos numeros tendra su array?(10 max)");
-        size=sc.nextInt();
-        } while (size>10);
-        int numeros[]=new int [size];
+        do {
+            System.out.println("Cuantos numeros tendra su array?(10 max)");
+            size = sc.nextInt();
+            valsize=fvalsize(size);
+        } while (valsize==false);
+        int numeros[] = new int[size];
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Introduzca numero");
             numeros[i] = sc.nextInt();
-            for(int h=0;h<i;h++){
-                if (numeros[h]==numeros[i]){
+            for (int h = 0; h < i; h++) {
+                if (numeros[h] == numeros[i]) {
                     System.out.println("ERROR: Numero repetido");
                     System.out.println("Anote un numero valido");
                     i--;
@@ -42,4 +44,11 @@ public class Practica {
             System.out.println(numeros[i]);
         }
     }
+
+    public static boolean fvalsize(int size) {
+        if (size<0 || size>10)
+            return false;
+        return true;
+    }
+
 }
